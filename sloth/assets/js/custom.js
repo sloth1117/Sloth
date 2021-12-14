2c1477e091168dada25dc996b9924a8f713bd2ff
sloth1117 <sloth0106@gmail.com>
2021-12-14 16:08:12 +0900

    최초 업데이트
    21.12.14

---
+++ sloth/assets/js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  //뒤로가기
+  $("#pf_more button.back").click(function(){
+    history.back();
+  });
+
+  //포트폴리오 작업물 마우스오버
+  $(".work > li").mouseover(function(){
+    $(this).addClass("on");
+  });
+
+  $(".work > li").mouseout(function(){
+    $(this).removeClass("on");
+  });
+
+
+  //스킬 숫자카운트
+  $('.cl80').animateNumber( 
+    {
+      number: 80,
+    },
+    {
+      easing: 'swing',
+      duration: 6000
+    }
+    );
+
+    $('.cl70').animateNumber( 
+      {
+        number: 70,
+      },
+      {
+        easing: 'swing',
+        duration: 5000
+      }
+      );
+
+
+});
